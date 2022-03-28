c84c0f9abaf728a8a4669c7149d473ddfbb91549
Kirat <singhprabhkirat70@gmail.com>
2022-03-28 20:47:28 +0530

    First game with object oriented programming

--- script.js
+++ script.js
@@ -4,11 +4,32 @@ const c = canvas.getContext("2d");
 c.canvas.width = 700;
 c.canvas.height = 600;
 
-class spirit {
-    constructor (position, color, width, height) {
-        this.position = position;
-        this.color = color;
-        this.width = width;
-        this.height = height;
-    }
+class sprite {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.color = "red";
+    this.width = 60;
+    this.height = 150;
+  }
+
+  draw() {
+    c.fillStyle = this.color;
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+      console.log("Update")
+  }
 }
+
+const player = new sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  }});
+
+player.draw();
