6bd49a8f102e7d7b8f56d5686efc64bbc3f72fbd
Kirat <singhprabhkirat70@gmail.com>
2022-03-29 20:16:39 +0530

    First Game with object oriented programming

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 // Initalization
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
-c.canvas.width = 700;
-c.canvas.height = 600;
+canvas.width = 700;
+canvas.height = 600;
+const gravity = 0.5;
 
 class sprite {
-  constructor({ position, velocity }) {
+  constructor({ position, velocity}) {
     this.position = position;
-    this.color = "red";
-    this.width = 60;
+    this.width = 50;
     this.height = 150;
+    this.velocity = velocity;
   }
 
   draw() {
-    c.fillStyle = this.color;
+    c.fillStyle = "red";
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 
   update() {
-      console.log("Update")
+    this.draw()
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height >= canvas.height) {
+      this.velocity.y = 0;
+    } else this.velocity.y += gravity;
+
+
   }
 }
 
@@ -30,6 +38,65 @@ const player = new sprite({
   velocity: {
     x: 0,
     y: 0,
-  }});
+  },
+});
 
-player.draw();
+const enemy = new sprite({
+  position : {
+    x: 400,
+    y: 0
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  }
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  player.update()
+  enemy.update()
+}
+animate();
+ 
+window.addEventListener('keydown', (event)=>{
+  playerMovementWithKeys(event)
+  enemyMovementWithKeys(event)
+})
+function playerMovementWithKeys (key) {
+  switch (key.key) {
+    case "w":
+      player.velocity.y -= 15;
+      break;
+
+    case "a":
+      player.velocity.x = 6;
+      player.position.x -= player.velocity.x;
+      break;
+
+    case "d":
+      player.velocity.x = 6;
+      player.position.x += player.velocity.x;
+      break;
+  }
+}
+
+function enemyMovementWithKeys (key) {
+  switch (key.key) {
+    case "8":
+      enemy.velocity.y -= 15;
+      break;
+
+    case "4":
+      enemy.velocity.x = 2;
+      enemy.position.x -= enemy.velocity.x;
+      break;
+
+    case "6":
+      enemy.velocity.x = 5;
+      enemy.position.x += enemy.velocity.x;
+      break;
+  }
+}
